6ff26471f664beff85ae01ffe3a1c3fe4f093acb
yogeshmankari <mankariyogesh35431@gmail.com>
2024-09-10 12:23:54 +0530

    adding the project

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-500'>APP of react</h1>
+      <p>This is a basic React app using Vite or Create React App.</p>
+      
+    </div>
+  );
+};
+
+export default App;
